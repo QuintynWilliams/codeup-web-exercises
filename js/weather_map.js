9289ffc46254011275e8107839926d61191bb35e
Quintyn Williams <t.quin.williams@gmail.com>
2023-03-26 09:40:50 -0500

    code comments

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,5 @@
 
-// Global Variables
+// GLOBAL VARIABLES
 let $weatherDiv = $('#weather')
 let $currDiv = $('#currCast')
 let $currCity = $('#currCity')
@@ -8,80 +8,82 @@ let fiveCast = [];
 
 
 // EVENT LISTENERS
-window.addEventListener ('load', (event) => {
-    event.preventDefault();
-    let markers = document.querySelectorAll(".mapboxgl-marker")
-    markers.forEach(thing => {
-        thing.remove()
-    });
-    let start = 'BANDERA, TX'
-    $currCity.html(start);
-    fiveCast = []
-    currCast = [];
-    currWeather(start);
-    getWeather(start);
-    getMapData(start);
-})
+    window.addEventListener ('load', (event) => {
+        event.preventDefault();
+        let markers = document.querySelectorAll(".mapboxgl-marker")
+        markers.forEach(thing => {
+            thing.remove()
+        });
+        let start = 'BANDERA, TX'
+        $currCity.html(start);
+        fiveCast = []
+        currCast = [];
+        currWeather(start);
+        getWeather(start);
+        getMapData(start);
+    })
 
-document.getElementById('searchWeather').addEventListener('keypress', e => {
-    let markers = document.querySelectorAll(".mapboxgl-marker")
-    markers.forEach(thing => {
-        thing.remove()
-    });
-    const address = document.getElementById("searchWeather").value.toUpperCase();
-    if (e.key === 'Enter') {
-        $currCity.html(address);
+    document.getElementById('searchWeather').addEventListener('keypress', e => {
+        let markers = document.querySelectorAll(".mapboxgl-marker")
+        markers.forEach(thing => {
+            thing.remove()
+        });
+        const address = document.getElementById("searchWeather").value.toUpperCase();
+        if (e.key === 'Enter') {
+            $currCity.html(address);
+            fiveCast = [];
+            currCast = [];
+            currWeather(address);
+            getWeather(address);
+            getMapData(address);
+        }
+    })
+
+    map.on('dblclick', (e) => {
+        let markers = document.querySelectorAll(".mapboxgl-marker")
+        markers.forEach(thing => {
+            thing.remove()
+        });
+        let newMark = e.lngLat;
         fiveCast = [];
         currCast = [];
-        currWeather(address);
-        getWeather(address);
-        getMapData(address);
-    }
-})
-
-map.on('dblclick', (e) => {
-    let markers = document.querySelectorAll(".mapboxgl-marker")
-    markers.forEach(thing => {
-        thing.remove()
-    });
-    let newMark = e.lngLat;
-    fiveCast = [];
-    currCast = [];
-    reverseGeocode(newMark, MAPBOX_API_TOKEN).then(namedCity => {
-        $currCity.html(namedCity);
-        currWeather(namedCity);
-        getWeather(namedCity);
-        getMapData(namedCity)
-    })
-});
-
-map.on('mouseup', (e) => {
-    let markers = document.querySelectorAll(".mapboxgl-marker")
-    markers.forEach(thing => {
-        thing.remove()
-    });
-    let newMark = e.lngLat;
-    fiveCast = [];
-    currCast = [];
-    reverseGeocode(newMark, MAPBOX_API_TOKEN).then(namedCity => {
-        $currCity.html(namedCity);
-        currWeather(namedCity);
-        getWeather(namedCity);
-        geocode(namedCity, MAPBOX_API_TOKEN).then(coords => {
-            const newMarker = new mapboxgl.Marker({
-                draggable: true
-            })
-                .setLngLat(coords)
-                .addTo(map);
-            const newPopup = new mapboxgl.Popup()
-                .setHTML(`<p class="pin">${namedCity}</p>`);
-            newMarker.setPopup(newPopup);
+        reverseGeocode(newMark, MAPBOX_API_TOKEN).then(namedCity => {
+            $currCity.html(namedCity);
+            currWeather(namedCity);
+            getWeather(namedCity);
+            getMapData(namedCity)
         })
-    })
-});
+    });
 
-// Render Cloud Cover Icon
-const assignCover = (cover) => {
+    map.on('mouseup', (e) => {
+        let markers = document.querySelectorAll(".mapboxgl-marker")
+        markers.forEach(thing => {
+            thing.remove()
+        });
+        let newMark = e.lngLat;
+        fiveCast = [];
+        currCast = [];
+        reverseGeocode(newMark, MAPBOX_API_TOKEN).then(namedCity => {
+            $currCity.html(namedCity);
+            currWeather(namedCity);
+            getWeather(namedCity);
+            geocode(namedCity, MAPBOX_API_TOKEN).then(coords => {
+                const newMarker = new mapboxgl.Marker({
+                    draggable: true
+                })
+                    .setLngLat(coords)
+                    .addTo(map);
+                const newPopup = new mapboxgl.Popup()
+                    .setHTML(`<p class="pin">${namedCity}</p>`);
+                newMarker.setPopup(newPopup);
+            })
+        })
+    });
+
+
+// WEATHER SPECIFIC IMGs
+//  Assign cloud coverage icons
+    const assignCover = (cover) => {
     let imgCloud = '';
 
     if ((100 - cover) >= 70) {
@@ -95,7 +97,8 @@ const assignCover = (cover) => {
     return imgCloud
 }
 
-const assignBack = (cover) => {
+//  Assign cloud coverage background images
+    const assignBack = (cover) => {
     let bgImage = '';
 
     if ((100 - cover) >= 70) {
@@ -109,34 +112,37 @@ const assignBack = (cover) => {
 
 }
 
-// Create HTML String for Browser
-const renderCurrHTML = (currData) => {
+
+// HTML STRING RENDER
+//  Render current weather for #currCast
+    const renderCurrHTML = (currData) => {
     console.log(currData)
     let html = `<div class="currCard column justify-center align-center" style="background-image: url('/codeup-web-exercises/${assignBack(currData.clouds.all)}')">`;
         html +=    ` <span class="date">Current on: ${namedDayFromDay(currData.dt)}, ${formatTime(currData.dt)}</span>`;
-        html +=    ` <div id="curr-render-data" class="row align-bottom">`;
-        html +=       ` <p class="column"><span class="bold">Description:</span> ${currData.weather[0].description}`;
-        html +=       ` <p class="column"><span class="bold">Humidity:</span> ${currData.main.humidity} %`;
-        html +=       ` <p class="column"><span class="bold">Wind:</span> ${currData.wind.speed} mph ${windCardinalDirection(currData.wind.deg)}`;
-        html +=       ` <p class="column"><span class="bold">Pressure:</span> ${currData.main.pressure} hPa`;
+        html +=    ` <div id="curr-render-data" class="row align-center">`;
+        html +=       ` <p class="column light"><span class="bold">Description:</span> ${currData.weather[0].description}`;
+        html +=       ` <p class="column light"><span class="bold">Humidity:</span> ${currData.main.humidity} %`;
+        html +=       ` <p class="column light"><span class="bold">Wind:</span> ${currData.wind.speed} mph ${windCardinalDirection(currData.wind.deg)}`;
+        html +=       ` <p class="column light"><span class="bold">Pressure:</span> ${currData.main.pressure} hPa`;
         html +=       ` <div class="column weather-icon align-center">`;
         html +=           ` <img src="images/weather_map_imgs/sunny.svg" alt="poiadhf" height="60px" width="60px">`;
         html +=       `</div>`;
-        html +=       ` <span class="column temp ">${ parseInt(currData.main.temp)} °F</span>`;
+        html +=       ` <span class="column temp">${ parseInt(currData.main.temp)}°F</span>`;
         html +=   `</div>`;
         html += `</div>`;
     return html;
 }
 
-const renderWeatherHTML = (wetData) => {
+//  Render current weather for #weather
+    const renderWeatherHTML = (wetData) => {
     let html = `<div class="weatherCard column justify-center align-center" style="background-image: url('/codeup-web-exercises/${assignBack(wetData.clouds.all)}')">`;
         html +=     `<span class="date" >${namedDayFromDay(wetData.dt)}, ${formatTime(wetData.dt)}</span>`;
         html +=     `<div id="render-data" class="row">`;
         html +=         `<div class="info column justify-space-between align-left">`;
-        html +=             `<p> <span class="bold">Description:</span> ${wetData.weather[0].description}</p>`;
-        html +=             `<p> <span class="bold">Humidity:</span> ${wetData.main.humidity} %</p>`;
-        html +=             `<p> <span class="bold">Wind:</span> ${wetData.wind.speed} mph ${windCardinalDirection(wetData.wind.deg)}</p>`;
-        html +=             `<p> <span class="bold">Pressure:</span> ${wetData.main.pressure} hPa</p>`;
+        html +=             `<p class="light"> <span class="bold">Description:</span> ${wetData.weather[0].description}</p>`;
+        html +=             `<p class="light"> <span class="bold">Humidity:</span> ${wetData.main.humidity} %</p>`;
+        html +=             `<p class="light"> <span class="bold">Wind:</span> ${wetData.wind.speed} mph ${windCardinalDirection(wetData.wind.deg)}</p>`;
+        html +=             `<p class="light"> <span class="bold">Pressure:</span> ${wetData.main.pressure} hPa</p>`;
         html +=         `</div>`;
         html +=         `<div class="headWeather column justify-space-between align-center">`;
         html +=             `<div class="weather-icon">`;
@@ -149,8 +155,10 @@ const renderWeatherHTML = (wetData) => {
     return html;
 }
 
-// Outside HTML Concatonator
-function renderWeather(dataSet) {
+
+// HTML STRING CONCATENATOR
+//  Loops through getWeather objects for each weatherCard
+    function renderWeather(dataSet) {
     let html = '';
         for(let i = 0; i < dataSet.length; i++) {
             console.log(dataSet)
@@ -159,15 +167,17 @@ function renderWeather(dataSet) {
     return html;
 }
 
-function renderCurrWeather(currData) {
+//  Queues Current Weather specific HTML
+    function renderCurrWeather(currData) {
     let html = '';
         html += renderCurrHTML(currData);
     return html;
 }
 
 
-// Render Weather Data
-const getWeather = (cityName) => {
+// API REQUEST FOR WEATHER DATA
+//  Render Forecasted Weather
+    const getWeather = (cityName) => {
     geocode(cityName, MAPBOX_API_TOKEN).then(geoCode => {
         const cityLong = geoCode[0];
         const cityLat = geoCode[1];
@@ -187,9 +197,8 @@ const getWeather = (cityName) => {
     })
 }
 
-// Render Current Weather
-
-const currWeather = (currHome) => {
+//  Render Current Weather
+    const currWeather = (currHome) => {
     geocode(currHome, MAPBOX_API_TOKEN).then(currGeo => {
         const currLong = currGeo[0];
         const currLat = currGeo[1];
@@ -206,7 +215,7 @@ const currWeather = (currHome) => {
 }
 
 
-// Render MapBox Data
+// RENDER MAPBOX DATA
 const getMapData = (mapCity) => {
     geocode(mapCity, MAPBOX_API_TOKEN).then(coords => {
         const newMarker = new mapboxgl.Marker({
