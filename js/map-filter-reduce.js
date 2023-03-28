c68a03644f7ec42340ab8ca61799bf8ce745ad8a
Quintyn Williams <t.quin.williams@gmail.com>
2023-03-28 15:11:52 -0500

    mini exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -56,7 +56,6 @@
 //  Then calculate the average
 //     w/ previous func
     let avgExp = (totalExp/users.length)
-
     console.log(avgExp)
 
 //     as stand-alone func
@@ -84,6 +83,31 @@
 
     let uniqueLang = [...new Set(eachLang)]
     console.log(uniqueLang)
+
+//  MINI EXs
+    const smashers = ["Mario", "Donkey Kong", "Link", "Samus","Dark Samus", "Yoshi", "Kirby",
+        "Fox", "Pikachu", "Luigi", "Ness", "Captain Falcon", "Jigglypuff", "Peach", "Daisy",
+        "Bowser", "Ice Climbers", "Sheik", "Zelda", "Dr. Mario", "Pichu", "Falco", "Marth",
+        "Lucina", "Young Link", "Ganondorf", "Mewtwo", "Roy", "Chrom", "Mr. Game & Watch",
+        "Meta Knight", "Pit", "Dark Pit", "Zero Suit Samus", "Wario", "Snake", "Ike",
+        "Pokemon Trainer", "Squirtle", "Ivysaur", "Charizard", "Diddy Kong", "Lucas", "Sonic",
+        "King Dedede", "Olimar", "Lucario", "R.O.B.", "Toon Link", "Wolf", "Villager",
+        "Mega Man", "Wii Fit Trainer", "Rosalina & Luma", "Little Mac", "Greninja",
+        "Mii Brawler", "Mii Swordfighter", "Mii Gunner", "Palutena", "Pac-Man", "Robin",
+        "Shulk", "Bowser Jr.", "Duck Hunt", "Ryu", "Ken", "Cloud", "Corrin", "Bayonetta",
+        "Inkling", "Ridley", "Simon", "Richter", "King K. Rool", "Isabelle", "Incineroar",
+        "Piranha Plant", "Joker", "Hero", "Banjo & Kazooie", "Terry", "Byleth", "Min Min",
+        "Steve", "Sephiroth", "Pyra", "Mythra", "Kazuya", "Sora"];
+
+//  BONUS: Order these arrays alphabetically.
+//      Create an array with all the smash characters whose names contain only 5 letters.
+    let fiveLong = smashers.filter(n => n.length === 5).sort()
+    console.log(fiveLong)
+
+//      Create another array with all the smash characters whose names contain over 10 letters.
+    let tenLong = smashers.filter(n => n.length > 10).sort()
+    console.log(tenLong)
+
 
 
 })();
